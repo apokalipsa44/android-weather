5409db853d3197cc91a2f20adfcf6c328c25f265
apokalipsa44 <m_lachowicz@wp.pl>
2020-04-26 02:36:26 +0200

    goto sleep observer not working

--- app/quotes/src/main/java/com/michau/quotes/ui/quotes/QuotesActivity.kt
+++ app/quotes/src/main/java/com/michau/quotes/ui/quotes/QuotesActivity.kt
@@ -2,12 +2,30 @@ package com.michau.quotes.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.michau.quotes.R
+import com.michau.quotes.utilities.InjectorUtils
+import kotlinx.android.synthetic.main.activity_quotes.*
+import kotlin.collections.forEach as forEach1
 
 class QuotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quotes)
+        initUi()
     }
+
+    fun initUi() {
+        val factory = InjectorUtils.provideQuotesViewModelFavtory()
+        val viewModel = ViewModelProvider(this, factory).get(QuotesViewModel::class.java)
+
+        viewModel.getQuotes()
+
+    }
+
+
+
 }
+
